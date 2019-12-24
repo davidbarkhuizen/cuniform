41f2964ade981a6b5473151ad80dc20d090cb508
david barkhuizen <david.barkhuizen@gmail.com>
2019-12-24 11:58:48 +0200

    WIP: UI controller

--- frame.js
+++ frame.js
@@ -26,95 +26,224 @@ let canvas = null;
 let context2d = null;
 let state = new State();
 let selectionInfoPanel = null;
+let resetElement = null;
+let exportElement = null;
 
-const updateSelectionInfo = () => {
-	
-	const selectedNode = window.fdg.graph.vertices.find(
-		vertex => (vertex.isSelected == true)
-	)
-	
-	const selectedNodeInfoLabel = document.getElementById('selectedNodeInfoLabel');
-	const list = document.getElementById('selectedNodeInfoList');
-	
-	// clear current items
-	while (list.children.length > 0) {
-		list.removeChild(list.firstChild);
+class UIController {
+
+	constructor(
+		canvas, 
+		exportElement, 
+		resetElement
+	) {
+		this.canvas = canvas;
+		this.exportElement = exportElement;
+		this.resetElement = resetElement;
 	}
 
-	// no selection => discard old info
-	//
-	if (!selectedNode) {
-		selectedNodeInfoLabel.innerHTML = 'Click on a node to select...';
-	}
-	else {
-		selectedNodeInfoLabel.innerHTML = selectedNode.label;
+	onMouseOut = (event) => {
+		window.state.b0Down = false;
+		window.state.b1Down = false;
+		window.state.b2Down = false;
+	};
 
-		window.fdg.graph.neighbours(selectedNode)
-		.forEach(
-			neighbour => {
-				const neighbourString = neighbour.label;
+	onMouseMove = (event) => {
+		/*
+		record mouse movement, calc deltas
+		call self.force_directed_graph.move(d_x, d_y, d_z), passing deltas
+		*/
+		if (window.state.b0Down) {
+	
+	
+			const mxy = this.getMousePos(canvas, event);
+			const phasePos = window.fdg.wrapReverse(mxy, canvas.width, canvas.height);
+			window.state.lastB0DragPos = mxy;
 			
-				const item = document.createElement('li');
-				item.innerHTML = neighbourString;
-				
-				list.insertBefore(item, list.firstChild);
-			}
-		)
-	}
-};
+			window.fdg.graph.vertices
+				.filter(vertex => (vertex.isSelected == true))
+				.forEach(
+					vertex => {
+						vertex.position = {x:phasePos.x, y:phasePos.y};
+					}
+				)
+		}
+	};	
 
-const getMousePos = (cnvs, evt) => {
-
-    // get canvas position
-    //
-    let obj = cnvs;
-    let top = 0;
-    let left = 0;
-    while (obj && obj.tagName != 'BODY') {
-        top += obj.offsetTop;
-        left += obj.offsetLeft;
-        obj = obj.offsetParent;
-    }
- 
-    // return relative mouse position
-    //
-	return new Point2D(
-        evt.clientX - left + window.pageXOffset,
-        evt.clientY - top + window.pageYOffset
-	)
-};
-
-const onTimerTick = (event) => {
-	window.fdg.iterate(context2d, canvas.width, canvas.height);
-};
-
-const onMouseOut = (event) => {
-	window.state.b0Down = false;
-	window.state.b1Down = false;
-	window.state.b2Down = false;
-};
-
-const onMouseMove = (event) => {
-	/*
-	record mouse movement, calc deltas
-	call self.force_directed_graph.move(d_x, d_y, d_z), passing deltas
-	*/
-	if (window.state.b0Down) {
-
-
-		const mxy = getMousePos(canvas, event);
-		const phasePos = window.fdg.wrapReverse(mxy, canvas.width, canvas.height);
-		window.state.lastB0DragPos = mxy;
+	onMouseDown = (event) => {
+	
+		var mxy = this.getMousePos(
+			this.canvas, 
+			event
+		);
 		
-		window.fdg.graph.vertices
-			.filter(vertex => (vertex.isSelected == true))
+		if (event.button == 0) {
+			window.state.b0Down = true;		
+			window.state.b0ClickPos = mxy;    
+				
+			const selectionChanged = window.fdg.handleNodeSelectionAttempt(mxy, canvas.width, canvas.height);
+			if (selectionChanged == true)
+				this.updateSelectionInfo();
+		}
+		else if (event.button == 1)
+			window.state.b1Down = true;
+		else if (event.button == 2)
+			window.state.b2Down = true;	
+	}
+	
+	getMousePos = (cnvs, evt) => {
+
+		// get canvas position
+		//
+		let obj = cnvs;
+		let top = 0;
+		let left = 0;
+		while (obj && obj.tagName != 'BODY') {
+			top += obj.offsetTop;
+			left += obj.offsetLeft;
+			obj = obj.offsetParent;
+		}
+	 
+		// return relative mouse position
+		//
+		return new Point2D(
+			evt.clientX - left + window.pageXOffset,
+			evt.clientY - top + window.pageYOffset
+		)
+	};
+
+	onMouseUp = (event) => {
+	
+		if (event.button == 0) {
+			window.state.b0Down = false;
+			this.updateSelectionInfo();
+		}
+		else if (event.button == 1)
+			window.state.b1Down = false;
+		else if (event.button == 2)
+			window.state.b2Down = false;
+	}
+
+	// reset, export handlers
+
+	onExport = () => {
+		window.open(
+			canvas.toDataURL('image/png')
+		);
+	};
+
+	onReset = (event) => {
+	
+		const reset = confirm('Reset.\nAre You Sure ?');
+		if (reset == true) {
+			this.terminate()
+			this.initialize()
+		}
+
+		event.preventDefault();
+
+		return false
+	};
+
+	deregisterEventListeners = (canvas, exportElement, resetElement) => {
+		
+		// export link
+		//
+		exportElement.removeEventListener("click", this.onExport);
+		
+		// reset link
+		//
+		resetElement.removeEventListener("click", this.onReset);
+
+		// mouse
+		//
+		canvas.removeEventListener("mousemove", this.onMouseMove, false);
+		canvas.removeEventListener("mousedown", this.onMouseDown, false);
+		canvas.removeEventListener("mouseup", this.onMouseUp, false);
+		canvas.removeEventListener("mouseout", this.onMouseOut, false);	
+	}
+	
+	registerEventListeners = (canvas, exportElement, resetElement) => {
+		
+		// export link
+		//
+		exportElement.addEventListener("click", this.onExport);
+		
+		// reset link
+		//
+		resetElement.addEventListener("click", this.onReset);
+
+		// mouse
+		//
+		canvas.addEventListener("mousemove", this.onMouseMove, false);
+		canvas.addEventListener("mousedown", this.onMouseDown, false);
+		canvas.addEventListener("mouseup", this.onMouseUp, false);
+		canvas.addEventListener("mouseout", this.onMouseOut, false);	
+	}
+
+	updateSelectionInfo = () => {
+	
+		const selectedNode = window.fdg.graph.vertices.find(
+			vertex => (vertex.isSelected == true)
+		)
+		
+		const selectedNodeInfoLabel = document.getElementById('selectedNodeInfoLabel');
+		const list = document.getElementById('selectedNodeInfoList');
+		
+		// clear current items
+		while (list.children.length > 0) {
+			list.removeChild(list.firstChild);
+		}
+	
+		// no selection => discard old info
+		//
+		if (!selectedNode) {
+			selectedNodeInfoLabel.innerHTML = 'Click on a node to select...';
+		}
+		else {
+			selectedNodeInfoLabel.innerHTML = selectedNode.label;
+	
+			window.fdg.graph.neighbours(selectedNode)
 			.forEach(
-				vertex => {
-					vertex.position = {x:phasePos.x, y:phasePos.y};
+				neighbour => {
+					const neighbourString = neighbour.label;
+				
+					const item = document.createElement('li');
+					item.innerHTML = neighbourString;
+					
+					list.insertBefore(item, list.firstChild);
 				}
 			)
+		}
+	};
+
+	initialize = () => {
+
+		const width = body.offsetWidth;
+		const height = body.offsetHeight * 0.9;
+	
+		canvas.width = width;
+		canvas.height = height;
+	
+		window.state = new State();
+			
+		const gFactory = new GraphFactory();
+		const graph = gFactory.generateGraph(30, 2);
+		window.fdg = new ForceDirectedGraph(graph);
+	
+		this.registerEventListeners(this.canvas, this.exportElement, this.resetElement);
+	
+		timer = setInterval(onTimerTick, 50)
+	
+		this.updateSelectionInfo();
 	}
-};	
+	
+	terminate = () => {
+		clearInterval(timer)
+		timer = null
+		this.deregisterEventListeners(this.canvas, this.exportElement, this.resetElement)
+	}
+	
+}
 
 const calcCanvasXY = (event, canvas) => {
 	return {
@@ -123,73 +252,12 @@ const calcCanvasXY = (event, canvas) => {
 	};
 };
 
-const onMouseDown = (event) => {
-	
-	var mxy = getMousePos(
-		canvas, 
-		event
-	);
-	
-	if (event.button == 0) {
-		window.state.b0Down = true;		
-		window.state.b0ClickPos = mxy;    
-			
-    	const selectionChanged = window.fdg.handleNodeSelectionAttempt(mxy, canvas.width, canvas.height);
-    	if (selectionChanged == true)
-    		updateSelectionInfo();
-	}
-	else if (event.button == 1)
-		window.state.b1Down = true;
-	else if (event.button == 2)
-		window.state.b2Down = true;	
-}
+let timer = null
 
-const onMouseUp = (event) => {
-	
-	if (event.button == 0) {
-		window.state.b0Down = false;
-		updateSelectionInfo();
-	}
-	else if (event.button == 1)
-		window.state.b1Down = false;
-	else if (event.button == 2)
-		window.state.b2Down = false;
-}
 
-const deregisterMouseEventListeners = (canvas) => {
-	canvas.removeEventListener("mousemove", onMouseMove, false);
-	canvas.removeEventListener("mousedown", onMouseDown, false);
-	canvas.removeEventListener("mouseup", onMouseUp, false);
-	canvas.removeEventListener("mouseout", onMouseOut, false);	
-}
-
-const registerMouseEventListeners = (canvas) => {
-	canvas.addEventListener("mousemove", onMouseMove, false);
-	canvas.addEventListener("mousedown", onMouseDown, false);
-	canvas.addEventListener("mouseup", onMouseUp, false);
-	canvas.addEventListener("mouseout", onMouseOut, false);	
-}
-
-const initialize = () => {
-
-	const width = body.offsetWidth;
-	const height = body.offsetHeight * 0.9;
-
-	canvas.width = width;
-	canvas.height = height;
-
-	window.state = new State();
-		
-	const gFactory = new GraphFactory();
-	const graph = gFactory.generateGraph(30, 2);
-	window.fdg = new ForceDirectedGraph(graph);
-
-	registerMouseEventListeners(canvas);
-
-	setInterval(onTimerTick, 50)
-
-	updateSelectionInfo();
-}
+const onTimerTick = (event) => {
+	window.fdg.iterate(context2d, canvas.width, canvas.height);
+};
 
 const entrypoint = () => {				
 
@@ -235,24 +303,19 @@ const entrypoint = () => {
 		return
 	}
 
-	initialize()
+	exportElement = document.getElementById('export_canvas_link');
+	resetElement = document.getElementById('reset_link');
+
+	const controller = new UIController(canvas, exportElement, resetElement);
+	controller.initialize()
 };
 
-const export_canvas_clicked_handler = () => {
- 	window.open(
-		canvas.toDataURL('image/png')
-	);
-};
+// selection info panel drag handlers
 
-const reset_clicked_handler = () => {
-	
-	if (confirm('Reset.\nAre You Sure ?')) {
-
-		deregisterMouseEventListeners()
-		initialize()
-	}
-};
-
+// draggable="true" 
+// ondrag="onDrag(event)"
+// ondragstart="onDragStart(event)"
+// ondragend="onDragEnd(event)"
 
 let startDragScreenX = null
 let startDragScreenY = null
@@ -283,17 +346,10 @@ const onDrag = (event) => {
 
 	dragX = event.screenX - startDragScreenX;
 	dragY = event.screenY - startDragScreenY;
-
-	console.log(dragX, dragY)
 }
 
 const onDragEnd = (event) => {
 
-	console.log(selectionInfoPanel.style)
-
 	selectionInfoPanel.style.top = dragY + startTop;
 	selectionInfoPanel.style.left = dragX + startLeft;
-
-	// selectionInfoPanel.style.top = parseInt(selectionInfoPanel.style.top, 10) + dragY + 'px';
-	// selectionInfoPanel.style.left = parseInt(selectionInfoPanel.style.left, 10) + dragX + 'px';
 }
